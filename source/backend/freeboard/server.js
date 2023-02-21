0e19af023596a6990b0cee9b16ebbd96a6b0f537
bornjre <bornjre@gmail.com>
2023-02-22 03:20:53 +0545

    freeboard server load action

--- source/backend/freeboard/server.js
+++ source/backend/freeboard/server.js
@@ -1,3 +1,15 @@
 function action_load(params) {
-    
+    core.log("@loaded_start")
+
+    const [tkt, err] = plugkv.get_ticket({})
+    if (err) {
+        core.log("@err" + err)
+        return utils.err_response(err)
+    }
+
+    core.log("@loaded_end")
+
+    return utils.ok_response({
+        state_token: tkt,
+    })
 }
