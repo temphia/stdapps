52bfca1d6610c9341391297d4b5319eca18e533f
bornjre <bornjre@gmail.com>
2023-03-20 21:01:05 +0545

    connect to data room properly

--- source/frontend/src/entries/eventmap/service/service.ts
+++ source/frontend/src/entries/eventmap/service/service.ts
@@ -1,9 +1,12 @@
 import type { Environment } from "../../../lib";
-import type { AdminDataAPI } from "temphia-frontend/dist/cjs/apiv2/admin/data";
+import type { DataAPI } from "temphia-frontend/dist/cjs/apiv2/data";
+import type { ExecAM } from "temphia-frontend/dist/cjs/exec/exec_am";
+import type { Sockd, SockdMessage } from "temphia-frontend/dist/cjs/sockd";
 
 export class EventmapService {
   env: Environment;
-  data_api: AdminDataAPI;
+  data_api: DataAPI;
+  sockd: Sockd;
   constructor(env: Environment) {
     this.env = env;
   }
@@ -15,12 +18,21 @@ export class EventmapService {
       return;
     }
 
-    const dbtoken = resp.data["dbtoken"];
-    const eam = await this.env.GetExecApiManager();
+    const dbtoken = resp.data.data["dbtoken"];
+    
+
+    const eam: ExecAM | any = await this.env.GetExecApiManager();
+
+    console.log("@eam", eam);
+
     this.data_api = eam.new_data_api(dbtoken);
+    this.sockd = await eam.new_sockd_room_from_url(
+      this.data_api.sockd_url(),
+      this.handle_sockd
+    );
+  };
 
-    console.log("@service", this)
-
-
+  handle_sockd = (msg: SockdMessage) => {
+    console.log("@sockd_msg", msg);
   };
 }
