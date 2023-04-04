1587cfd2d78f967fc5b9d398cefc36245efa2c3e
bornjre <bornjre@gmail.com>
2023-04-05 02:04:56 +0545

    fix icons

--- source/frontend/src/entries/eventmap/events/event.svelte
+++ source/frontend/src/entries/eventmap/events/event.svelte
@@ -1,6 +1,12 @@
 <script>
   export let title;
   export let contents;
+
+  export let onClick = () => {
+
+  }
+
+
 </script>
 
 <div class="rounded shadow flex flex-col p-2 cursor-pointer">
@@ -16,16 +22,15 @@
     </p>
 
     <div>
-      <button
-        class="bg-blue-50 hover:bg-gray-500 rounded  font-bold text-gray-700 text-sm px-2"
+      <button on:click={onClick}
+        class=" hover:bg-gray-50 rounded font-bold text-gray-700 text-sm p-1"
       >
-        🔺
+      <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+      </svg>
       </button>
-      <button
-        class="bg-blue-50 hover:bg-gray-500 rounded  font-bold text-gray-700 text-sm px-2"
-      >
-        🟦
-      </button>
+
     </div>
   </div>
 </div>
--- source/frontend/src/entries/eventmap/events/index.svelte
+++ source/frontend/src/entries/eventmap/events/index.svelte
@@ -1,11 +1,18 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Event from "./event.svelte";
   export let event_types = [];
   export let events = [];
+
+  const dispatch = createEventDispatcher();
 </script>
 
 <div class="p-1 overflow-auto">
   {#each events as evnt}
-    <Event contents={evnt.contents} title={evnt.title} />
+    <Event
+      contents={evnt.contents}
+      title={evnt.title}
+      onClick={() => dispatch("click_event", evnt)}
+    />
   {/each}
 </div>
