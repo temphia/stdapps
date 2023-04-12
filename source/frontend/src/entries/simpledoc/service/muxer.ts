696291e86e37d7ccd40e55eef16c118ff7651ff7
bornjre <bornjre@gmail.com>
2023-04-13 04:53:55 +0545

    implement sockd muxer

--- source/frontend/src/entries/simpledoc/service/muxer.ts
+++ source/frontend/src/entries/simpledoc/service/muxer.ts
@@ -1,6 +1,61 @@
-import type { SockdMessage } from "temphia-frontend/dist/cjs/sockd";
+import type { Sockd, SockdMessage } from "temphia-frontend/dist/cjs/sockd";
 
-export interface Muxer {
-  add_callback(docid: string, cb: (msg: SockdMessage) => void): void;
-  remove_callback(docid: string): void;
+export type sockd_builder = (
+  ticket: string,
+  callback: (msg: SockdMessage) => void
+) => Promise<Sockd>;
+
+export interface DocMessage {
+  type: string;
+  docid: string;
+  yjs_data: string;
+}
+
+export class SockdMuxer {
+  sockd: Sockd;
+  ticket: string;
+  builder: sockd_builder;
+
+  subs: Map<string, (msg: DocMessage) => void>;
+
+  constructor(ticket: string, builder: sockd_builder) {
+    this.ticket = ticket;
+    this.builder = builder;
+    this.subs = new Map();
+  }
+
+  init = async () => {
+    this.sockd = await this.builder(this.ticket, this.handle);
+  };
+
+  send_yjs_message = (docid: string, yjs_data: string) => {
+    this.sockd.SendBroadcast({
+      yjs_data,
+      docid,
+      type: "yjs",
+    });
+  };
+
+  add_callback = (docid: string, cb: (msg: DocMessage) => void): void => {
+    this.subs.set(docid, cb);
+  };
+
+  remove_callback = (docid: string): void => {
+    this.subs.delete(docid);
+  };
+
+  private handle = (msg: SockdMessage) => {
+    const inner = msg.payload as DocMessage;
+
+    switch (inner.type) {
+      case "yjs":
+        const handler = this.subs.get(inner.docid);
+        if (handler) {
+          handler(inner);
+        }
+        break;
+      default:
+        break;
+    }
+  };
 }
