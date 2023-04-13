7db90da35b21285e978ddb16aa9f4b9b0cd2cc6a
bornjre <bornjre@gmail.com>
2023-04-14 04:37:25 +0545

    add css assets in brpint bundle

--- source/frontend/src/entries/simpletasks/task_data/_task_edit.svelte
+++ source/frontend/src/entries/simpletasks/task_data/_task_edit.svelte
@@ -13,16 +13,4 @@
   $: console.log("@editor", data);
 </script>
 
-<svelte:head>
-  <link rel="stylesheet" href="//cdn.quilljs.com/1.3.6/quill.snow.css" />
-  <link
-    rel="stylesheet"
-    href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css"
-  />
-  <link
-    rel="stylesheet"
-    href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css"
-  />
-</svelte:head>
-
 <Quill contents={data["content"]} bind:getHTML />
