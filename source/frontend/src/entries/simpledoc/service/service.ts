d1010b8d52e8ddb5385f400dc7413d319e23c3b3
bornjre <bornjre@gmail.com>
2023-04-13 06:40:28 +0545

    add  state api

--- source/frontend/src/entries/simpledoc/service/service.ts
+++ source/frontend/src/entries/simpledoc/service/service.ts
@@ -1,3 +1,4 @@
+import type { AdminPlugStateTktAPI } from "temphia-frontend/dist/cjs/apiv2";
 import type { Environment } from "../../../lib";
 import { SockdMuxer } from "./muxer";
 
@@ -25,5 +26,55 @@ export class SimpleDocService {
     await this.muxer.init();
 
     return true;
+  };
+}
+
+export class DocApi {
+  env: Environment;
+  state_api: AdminPlugStateTktAPI;
+
+  constructor(env: Environment) {
+    this.env = env;
+  }
+
+  list_docs = () => {
+    return this.state_api.query({
+      tag1s: ["doc"],
+    });
+  };
+
+  add_doc = async (id: string, data: any) => {
+    const resp = await this.state_api.add(id, JSON.stringify(data), {
+      tag1: "doc",
+    });
+    if (!resp.ok) {
+      return resp;
+    }
+
+    const resp1 = await this.state_api.add(`${id}-data`, "{}", {
+      tag1: "doc-data",
+    });
+    if (!resp1.ok) {
+      console.log("Err creating meta table", resp1);
+    }
+
+    return resp;
+  };
+
+  remove_doc = async (id: string) => {
+    await this.state_api.delete(`${id}-data`);
+    return this.state_api.delete(id);
+  };
+
+  update_doc = (id: string, data: any) => {
+    return this.state_api.update(id, JSON.stringify(data));
+  };
+
+  get_doc_data = (id: string) => {
+    return this.state_api.get(`${id}-data`);
+  };
+
+  update_doc_data = (id: string, data: any) => {
+    return this.state_api.update(`${id}-data`, JSON.stringify(data));
   };
 }
